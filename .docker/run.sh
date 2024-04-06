e7b77757edb6c10a653f7b6be4afc6c240360b2f
ShunjiHashimoto <h.shunji1206@gmail.com>
2024-04-06 18:10:14 +0900

    raspiのGUIをノートPCに表示する

--- .docker/run.sh
+++ .docker/run.sh
@@ -4,8 +4,9 @@ DOCKER_IMAGE="leg_tracker_ros2:latest"
 CONTAINER_NAME="leg_tracker_ros2"
 
 docker run --rm -it \
-    --env="DISPLAY=$DISPLAY" \
+    --env DISPLAY=localhost:10.0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ~/.Xauthority:/root/.Xauthority:ro \
     --volume="${HOME}/ros2_ws:/root/ros2_ws" \
     --name=$CONTAINER_NAME \
     --privileged \
