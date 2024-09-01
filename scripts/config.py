67ef24447b85d926f6dd6646da4864a1f1aa3f4b
ShunjiHashimoto <h.shunji1206@gmail.com>
2024-09-01 14:44:46 +0900

    パラメータをconfig.pyに移動

---
+++ scripts/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class LegTrackerParam:
+    # 重みの定義
+    id_weight = 0.5
+    distance_weight = 0.0 
+    speed_weight = 2.0
+    static_penalty = -0.8 # 静的オブジェクトに対するペナルティ
+    single_leg_penalty = -1.0 # 片足のみしか検出しない場合は-0.5 のペナルティを追加
